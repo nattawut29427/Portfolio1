856c180119abe2b6b1dbecfdda0a87ee4ee43261
NATTAWUT_IN <150765583+nattawut29427@users.noreply.github.com>
2024-11-27 23:36:58 +0700

    Contract Page

--- app/(default)/contrack/page.tsx
+++ app/(default)/contrack/page.tsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from "react";
 
 function Contrack() {
+
+   
   return (
-    <div></div>
-  )
+    <div className="font-body min-h-screen flex flex-col justify-between text-white">
+      <main className="flex flex-col items-center gap-16">
+        <h1 className="text-3xl font-bold mt-40">CONTACT</h1>
+        <div className="text-center text-3xl gap-y-10 flex flex-col ">
+          <h1 className="">EMAIL</h1>
+          <h1>GIT HUB</h1>
+          <h1>LINK IN</h1>
+          <h1>INTRAGRAM</h1>
+        </div>
+      </main>
+    </div>
+  );
 }
 
-export default Contrack
+export default Contrack;
